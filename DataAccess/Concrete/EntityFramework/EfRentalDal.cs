171f1629491a8daeabe0902b1b1216a72a5d53cb
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-02-13 11:45:16 +0300

    Added new entites and their dals

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, CarRentalDbContext>, IRentalDal
+    {
+    }
+}
